ee634128f2e22538c22921f81483ecd2fb813082
em-ep <maxwell.epstein@columbia.edu>
2026-02-07 21:56:43 +0000

    verified code on hardware

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Raspberry Pi 5 code to control plasmaPico SPA driver
+Raspberry Pi 5 code to control plasmaPico SPA driver for the CUTE tokamak at Columbia University
--- test.py
+++ test.py
@@ -1,2 +1,20 @@
-import gpiod
-print("hi")
+from relay_controller import CapRelay
+from time import sleep
+
+relay = CapRelay([22, 23, 24, 25], [1, 1, 0, 0], [0, 0, 1, 1])
+
+relay.set_state("charge")
+
+sleep(1)
+
+relay.set_state("dump")
+
+sleep(1)
+
+relay.set_state("null")
+
+sleep(1)
+
+relay.close_all()
+
+sleep(1)
